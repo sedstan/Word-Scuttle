5e92533024f122f2a79306c21212ff457573f98a
sedstan <sedky.stanley.said@gmail.com>
2015-10-09 15:26:23 +0100

    Psuedo coding.

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// *****************************************************
+// OBJECTIVE
+// ******************************************************
+// 
+// The game is scrabble. The player is given seven letters to make a word. The player has one mintue to make as many words as possible. 
+
+// ********************************************************
+// LOGIC
+// ********************************************************
+// 
