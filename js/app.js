6d16240fa1149d1ec56f640a115ba6c9983f5293
sedstan <sedky.stanley.said@gmail.com>
2015-10-09 17:29:42 +0100

    basic html-css design.

--- js/app.js
+++ js/app.js
@@ -7,9 +7,56 @@
 // ********************************************************
 // LOGIC
 // ********************************************************
-// Random  7 letters given from an array of 26. (This is at the top). These are 7 squares for the individual tile.
-// The timer once the player "clicks" the first letter.
-// player "clicks and drags letter" to the display window.
+// Random  7 LETTERS given from an array of 26. (This is at the top). These are 7 squares for the individual tile.
+// The TIMER once the player "clicks" the first letter.
+// player "clicks and drags letter" to the DISPLAY WINDOW.
 // The display window will be in the center. It will have 7 spaces. 
-// Below that is a play button.  and a skip button. The skip button will subtract 10 points from your total score if you press it.
+// Below that is a PLAY BUTTON and a SKIP BUTTON. The skip button will SUBTRACT 10 POINTS from your total score if you press it.
 // There will be a display to keep your total score after one minute.
+// After time expires, the player is alerted to their score and will be asked to play again by.
+// ***************************************************************
+// DESIGN - PSEUDO
+// ******************************************************************
+// Page on load: fade in title screen to the game board.
+// BOARD LAYOUT:
+//      TOP: WILL SHOW THE FORMED WORD.
+//              1. This will be a div class of top.
+//                  Each square should be an empty array [0]; 
+//             2.    
+//
+//       MIDDLE: WILL HAVE THE PLAYER'S RANDOMIZED TILES. TIMER AND SCORE WILL BE BELOW THAT.
+//       
+//              1.Div class of middle.
+// 
+//       BOTTOM: WILL HAVE THE TWO BUTTONS.
+                  // Play button will have sound when click.
+                  // Skip button will have a sound when clicked.
+                  // 
+   
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+
+window.onlaod =function({
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
