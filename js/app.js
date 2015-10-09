6454c997d59c9876af96f27a032a9b480d80cb31
sedstan <sedky.stanley.said@gmail.com>
2015-10-09 16:04:37 +0100

    more psuedo code.

--- js/app.js
+++ js/app.js
@@ -7,5 +7,9 @@
 // ********************************************************
 // LOGIC
 // ********************************************************
-// Random  7 letters given from an array of 26.
+// Random  7 letters given from an array of 26. (This is at the top). These are 7 squares for the individual tile.
 // The timer once the player "clicks" the first letter.
+// player "clicks and drags letter" to the display window.
+// The display window will be in the center. It will have 7 spaces. 
+// Below that is a play button.  and a skip button. The skip button will subtract 10 points from your total score if you press it.
+// 
