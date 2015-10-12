f5f5a483493a6b1967634ce2511dc05b3dede430
sedstan <sedky.stanley.said@gmail.com>
2015-10-12 11:05:25 +0100

    Adding the tiles to the board

--- js/app.js
+++ js/app.js
@@ -1,29 +1,66 @@
 var letters = {
-  "blank": {score: 0, count: 2},
-  "a": {score: 1, count: 9},
-  "b": {score: 3, count: 2},
-  "c": {score: 3, count: 2},
-  "d": {score: 2, count: 4},
-  "e": {score: 1, count: 12},
-  "f": {score: 4, count: 2},
-  "g": {score: 2, count: 3},
-  "h": {score: 4, count: 2},
-  "i": {score: 1, count: 9},
-  "j": {score: 8, count: 1},
-  "k": {score: 5, count: 1},
-  "l": {score: 1, count: 4},
-  "m": {score: 3, count: 2},
-  "n": {score: 1, count: 6},
-  "o": {score: 1, count: 9},
-  "p": {score: 3, count: 2},
-  "q": {score: 10, count: 1},
-  "r": {score: 1, count: 6},
-  "s": {score: 1, count: 4},
-  "t": {score: 1, count: 6},
-  "u": {score: 1, count: 4},
-  "v": {score: 4, count: 2},
-  "w": {score: 4, count: 2},
-  "x": {score: 8, count: 1},
-  "y": {score: 4, count: 2},
-  "z": {score: 10, count: 1},
+  "blank": {letter: "", points: "", count: 2},
+  "a": {letter: "a", points: 1, count: 9},
+  "b": {letter: "b", points: 3, count: 2},
+  "c": {letter: "c", points: 3, count: 2},
+  "d": {letter: "d", points: 2, count: 4},
+  "e": {letter: "e", points: 1, count: 12},
+  "f": {letter: "f", points: 4, count: 2},
+  "g": {letter: "g", points: 2, count: 3},
+  "h": {letter: "h", points: 4, count: 2},
+  "i": {letter: "i", points: 1, count: 9},
+  "j": {letter: "j", points: 8, count: 1},
+  "k": {letter: "k", points: 5, count: 1},
+  "l": {letter: "l", points: 1, count: 4},
+  "m": {letter: "m", points: 3, count: 2},
+  "n": {letter: "n", points: 1, count: 6},
+  "o": {letter: "o", points: 1, count: 9},
+  "p": {letter: "p", points: 3, count: 2},
+  "q": {letter: "q", points: 10, count: 1},
+  "r": {letter: "r", points: 1, count: 6},
+  "s": {letter: "s", points: 1, count: 4},
+  "t": {letter: "t", points: 1, count: 6},
+  "u": {letter: "u", points: 1, count: 4},
+  "v": {letter: "v", points: 4, count: 2},
+  "w": {letter: "w", points: 4, count: 2},
+  "x": {letter: "x", points: 8, count: 1},
+  "y": {letter: "y", points: 4, count: 2},
+  "z": {letter: "z", points: 10, count: 1},
+}
+
+var playersHand = [];
+var bag = createBag();
+
+$(function(){
+  $('.start').on("click", play);
+});
+
+function play(){
+  $('.player')
+  dealLetters();
+}
+
+function createBag(){
+  var bag = [];
+  for (letter in letters) {
+    for (var i = 0; i < letters[letter].count; i++){
+      bag.push(letter);
+    }
+  }
+  return bag;
+}
+
+function dealLetters() {
+  while (playersHand.length !== 7) {
+    var randomIndex = Math.floor(Math.random()*bag.length);
+    var letter      = bag[randomIndex];
+    var letterObj   = letters[letter];
+
+    // remove element from the bag
+    bag.splice(randomIndex, 1);
+
+    playersHand.push(letterObj);
+
+    $('.player').append('<div class="tile">'+ letterObj.letter + '<div class="points">'+ letterObj.points + '</div></div>');
+  }
 }
